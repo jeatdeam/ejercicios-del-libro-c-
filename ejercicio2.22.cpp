0dda3a25bc2804426a0f0593a408c1890f996dd2
jeatdeam <48858548+jeatdeam@users.noreply.github.com>
2019-04-01 19:27:36 -0500

    Add files via upload

---
+++ ejercicio2.22.cpp
@@ -0,0 +1,24 @@
+﻿'Project6.exe' (Win32): 'C:\Users\Usuario\source\repos\Project6\Debug\Project6.exe' cargado. Símbolos cargados.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\ntdll.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\kernel32.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\KernelBase.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\apphelp.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\vcruntime140d.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\ucrtbased.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\msvcp140d.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\msvcp140d.dll' descargado
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\ucrtbased.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\ucrtbased.dll' descargado
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\msvcp140d.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\kernel.appcore.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\msvcrt.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\rpcrt4.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\sspicli.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\cryptbase.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\bcryptprimitives.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+'Project6.exe' (Win32): 'C:\Windows\SysWOW64\sechost.dll' cargado. No se encuentra el archivo PDB o no se puede abrir.
+El subproceso 0x2d38 terminó con código 0 (0x0).
+El subproceso 0x27c8 terminó con código 0 (0x0).
+El subproceso 0x3814 terminó con código 0 (0x0).
+El subproceso 0x1064 terminó con código 0 (0x0).
+El programa '[3828] Project6.exe' terminó con código 0 (0x0).
